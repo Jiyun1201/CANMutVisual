ee2d5a47a43d35e1047531f2bd52d6bafbced078
Karen Won <karenwon@Karens-MacBook-Pro.local>
2021-11-21 19:18:05 -0500

    initial_

---
+++ R/countMut.R
@@ -0,0 +1,38 @@
+#' Returns gene name and frequency of each gene types
+#'
+#' counMut function uses cosImport function in cosImport.R file to read from
+#' cosmic file and uses that dataframe to extract IDsymbol column and return the
+#' occurrence frequency of each gene types.
+#'
+#' @param mutFile A .tsv file containing COSMIC point mutations
+#' @param hgncFile A .tsv file with complete approved human gene nomenclature IDs
+#'
+#' @return Returns dataframe with gene names and frequency.
+#'
+#' @example
+#' # Using data files available in the package: "MutantData.tsv" and "HGNC.tsv"
+#'
+#' # countMut("Data/MutantData.tsv", "Data/HGNC.tsv")
+#'
+#' @export
+############################################################################################
+
+# source cosImport.R file to use the cosImport function in this script
+source("R/cosImport.R")
+
+countMut <- function(mutFile, hgncFile){ #Two input files to process cosimport function
+
+  # use cosImport function to get total data read from cosmic file
+  dataImp <- cosImport(mutFile, hgncFile)
+
+  # get IDsymbol column
+  symbol_gene <- dataImp$IDsymbol
+
+  # count the number of gene ID occurences and present as data frame with two columns
+  gene_freq <- as.data.frame(table(symbol_gene))
+
+  return (gene_freq)
+}
+#[END]
+
+
